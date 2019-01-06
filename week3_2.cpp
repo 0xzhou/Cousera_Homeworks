4a36e29990bee3a4f5fe480ebe637683ce956f82
Mingy2018 <39735862+Mingy2018@users.noreply.github.com>
2019-01-06 15:55:33 +0100

    Update week3_2.cpp

--- week3_2.cpp
+++ week3_2.cpp
@@ -3,9 +3,10 @@ using namespace std;
 class Sample {
 public:
 	int v;
-	Sample(int n):v(n) {}
+	Sample(int n):v(n)//构造函数初始化列表
+	{}
 	
-	Sample(Sample& a) {
+	Sample(Sample& a) {//返回值为Sample构造函数
 		v = 2 * a.v;
 	}
 };
