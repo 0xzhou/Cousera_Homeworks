1654d9f56b9e649664844317d896c74e7b29e6c9
Mingy2018 <605329720@qq.com>
2019-03-28 22:09:28 +0100

    Review of Operator Overloading

--- w4.1.1_运算符重载为普通函数.cpp
+++ w4.1.1_运算符重载为普通函数.cpp
@@ -1,4 +1,3 @@
-#include<iostream>
 
 class Complex {
 public:
@@ -6,16 +5,17 @@ public:
 	real = r;
     imaginary=i;
 	}
-	double real;
-	double imaginary;
+	double real;// real part
+	double imaginary; // imaginary part
 };
 
-Complex operator+(const Complex &a, const Complex & b)
+Complex operator+(const Complex & a, const Complex & b)
 {
 	return Complex(a.real + b.real, a.imaginary + b.imaginary);
-}
+} //"类名（参数表）"就代表一个对象
 int main() {
 	Complex a(1, 2), b(2, 3), c;
 	c = a + b;//调用了operator+(a,b)函数
 }
 
+//重载为普通函数时，参数个数为运算符目数？？
--- w4.1.2运载符重载为成员函数.cpp
+++ w4.1.2运载符重载为成员函数.cpp
@@ -1,18 +1,19 @@
-#include<iostream>
 
 class Complex {
 public:
 	Complex(double r = 0.0, double m = 0.0):
-		real(r), imaginary(m) {}// 5,6行是什么意思？
+		real(r), imaginary(m) {}// constructor
 	Complex operator+ (const Complex &);
 	Complex operator-(const Complex &);
 private:
 	double real;
 	double imaginary;
 };
+// Overloaded addition operator
 Complex Complex::operator+(const Complex & operand2) {
 	return Complex(real + operand2.real, imaginary + operand2.imaginary);
 }
+// Overloaded subtraction operator
 Complex Complex::operator-(const Complex & operand2) {
 	return Complex(real - operand2.real, imaginary - operand2.imaginary);
 }
@@ -22,5 +23,6 @@ int main() {
 	x = y + z;// x=y.operator+(z);
 	x = y - z;// x=y.operator-(z)
 	return 0;
-
 }
+
+//重载为成员函数时，参数个数为运算符目数减一
